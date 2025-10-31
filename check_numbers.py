1f695a8d4eac5f8f94ad6ef18d33af11493934d2
basmalaktesh-a11y <basmalaktesh@gmail.com>
2025-10-31 20:48:05 +0200

    Update check_numbers.py

--- check_numbers.py
+++ check_numbers.py
@@ -1,14 +1,7 @@
-def all_positive(numbers):
-    """
-    تفحص إذا كانت كل الأرقام موجبة.
-    """
-    for n in numbers:
-        if n <= 0:
-            return False
-    return True
+def check_number(num):
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
 
-
-# تجربة
-nums = [3, 7, -2, 5]
-print("القائمة:", nums)
-print("هل جميعها موجبة؟", all_positive(nums))
+print(check_number(7))
